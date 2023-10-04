53f3f483e6eb9c48f48e0df7741240370b4d7feb
paleblood <paleblood@localhost>
2023-10-04 15:47:42 -0500

    ShapeInterpreter

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,60 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		System.out.println(shape);
+		
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		
+		Color color = new Color(r, g, b);
+		StdDraw.setPenColor(color);
+		StdDraw.setPenRadius(0.015);
+		
+		boolean filled = in.nextBoolean();
+		
+		if(shape.equals("ellipse")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double semiMajorAxis = in.nextDouble();
+			double semiMinorAxis = in.nextDouble();
+			if (filled == true) {
+				StdDraw.filledEllipse(x, y, semiMajorAxis, semiMinorAxis);
+			}
+			else {
+				StdDraw.ellipse(x, y, semiMajorAxis, semiMinorAxis);
+			}
+		}
+		else if (shape.equals("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double semiHalfWidth = in.nextDouble();
+			double semiHalfHeight = in.nextDouble();
+			if (filled == true) {
+				StdDraw.filledRectangle(x, y, semiHalfWidth, semiHalfHeight);
+			}
+			else {
+				StdDraw.rectangle(x, y, semiHalfWidth, semiHalfHeight);
+			}
+		}
+		else if (shape.equals("triangle")) {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			if (filled == true) {
+				double[] x = {x1, x2, x3};
+				double[] y = {y1, y2, y3};
+				StdDraw.filledPolygon(x, y);
+			}
+			else {
+				double[] x = {x1, x2, x3};
+				double[] y = {y1, y2, y3};
+				StdDraw.polygon(x, y);
+			}
+		}
 	}
 }
